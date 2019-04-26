abc3a762f28cfe3b1e271927f358104b943e124e
cw0 <element4emerica@gmail.com>
2019-04-25 23:09:53 -0400

    stubbing

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -1,5 +1,16 @@
-const longestCommonPrefix = (x) => {
-	
+//find the longest common prefix given an array of strings
+
+const longestCommonPrefix = (input) => {
+  const size = input.length;
+  
+}
+
+class letterNode {
+  constructor(value) {
+    this.value = value;
+    this.count = 0;
+    this.next = null;
+  }
 }
 
 
