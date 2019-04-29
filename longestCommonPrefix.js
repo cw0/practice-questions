e875f4e978230a89b4c3bc5b053aa34e22448434
cw0 <element4emerica@gmail.com>
2019-04-29 08:52:16 -0400

    longestCommonPrefix

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -1,16 +1,40 @@
-//find the longest common prefix given an array of strings
-
+// find the longest common prefix given an array of strings
 const longestCommonPrefix = (input) => {
+  if (input.length === 0) return '';
+  if (input.length === 1) return input[0];
+
   const size = input.length;
-  
-}
+  const prefixes = [];
+  let shortest = Number.MAX_VALUE;
 
-class letterNode {
-  constructor(value) {
-    this.value = value;
-    this.count = 0;
-    this.next = null;
+  for (let word of input) {
+    if (word === '') return '';
+    
+    const letterArray = word.split('');
+    if (letterArray.length < shortest) {
+      shortest = letterArray.length;
+    }
+
+    letterArray.map((letter, index) => {
+      prefixes[index] ? prefixes[index].push(letter) : prefixes[index] = [letter];
+    });
   }
+
+  let prefix = '';
+
+  for (let i = 0; i < shortest; i++) {
+    if (prefixes[i].reduce((a,b) => {
+      return a === b ? a : NaN;
+    })) {
+      prefix = prefix + prefixes[i][0];
+    } else {
+      return prefix;
+    }
+  }
+
+  return prefix;
 }
 
+longestCommonPrefix(['c', 'c']);
 
+//console.log(longestCommonPrefix(["c","c"]));
