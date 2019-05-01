51650e33cd513405cbe9819c795d63a8310cb2aa
chris-PC\chris <cwilson8706@gmail.com>
2019-04-30 23:00:09 -0400

    add matching parenthesis

---
+++ matchingParenthesis.js
@@ -0,0 +1,56 @@
+//given a string of parenthesis characters, determine if they are valid
+const isValid = (input) => {
+  let openingChars = [];
+  let isValidParenthesis = true;
+  const canClose = (char) => {
+    if (openingChars.length === 0) return false;
+
+    let openChar = openingChars.pop();
+    switch (char) {
+      case ')': {
+        if (openChar === '(') return true;
+        break;
+      }
+      case '}': {
+        if (openChar === '{') return true;
+        break;
+      }
+      case ']': {
+        if (openChar === '[') return true;
+        break;
+      }
+      default: {
+        return false;
+      }
+    }
+    return false;
+  }
+  const checkCharacter = (char) => {
+    switch (char) {
+      case '(': 
+      case '[':
+      case '{': {
+        openingChars.push(char);
+        break;
+      }
+      default: {
+        isValidParenthesis = canClose(char);
+        break;
+      }
+    }
+  };
+
+  let charArray = input.split('');
+  let i = 0;
+
+  while (i < charArray.length && isValidParenthesis) {
+    checkCharacter(charArray[i]);
+    i++;
+  }
+
+  if (openingChars.length > 0) return false;
+
+  return isValidParenthesis;
+}
+
+console.log(isValid("{{)}"));
