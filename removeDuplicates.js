b03fd81fc6e7ead6fafa15346dfbe729ff166db7
cw0 <element4emerica@gmail.com>
2019-05-08 13:52:09 -0400

    add starter for removeDuplicates (incomplete)

---
+++ removeDuplicates.js
@@ -0,0 +1,11 @@
+// remove duplicates from a sorted array in line.
+// do not assign another array and return the length of the unique array.
+// elements set after the length of the unique array do not matter.
+const removeDuplicates = (input) => {
+  let length = 0;
+  let current = null;
+  
+  for (let i = 0; i < input.length; i++) {
+
+  }
+}
