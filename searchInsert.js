238cdc4ff39ccf1bd28dd687899bba798b6bb623
cw0 <element4emerica@gmail.com>
2019-05-12 01:45:01 -0400

    add search insert

--- searchInsert.js
+++ searchInsert.js
@@ -1,6 +1,10 @@
+//given a sorted array and a target value, find the index of the value if it
+//exists in the array or the index of where it would be inserted
+//protip: turns out its the same index either way.
+
 const searchInsert = (input, value) => {
   let i = 0;
-  while (input[i] < value) i++;
+  while (input[i] < value) i++; //undefined < value
   return i;
 }
 
