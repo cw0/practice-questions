7756680b24cdbd01a70fc8c4d5c210a9ae5f1874
chris-PC\chris <cwilson8706@gmail.com>
2019-12-02 19:40:43 -0500

    add strstr (incomplete)

---
+++ easy/strStr/strStr.js
@@ -0,0 +1,9 @@
+const strStr = (haystack, needle) => {
+  if (haystack.length === 0 || haystack.length < needle.length) return 0;
+  
+  let i = 0;
+  let j = 0;
+  while (i < haystack.length) {
+    if (haystack[i])
+  }
+};
