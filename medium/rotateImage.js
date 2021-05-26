ad2b3398a1202d7c8371ddf37ef6779dab3bc17e
cw0 <cwilson8706@gmail.com>
2021-05-26 17:40:07 -0400

    rotate an image complete

--- medium/rotateImage.js
+++ medium/rotateImage.js
@@ -1,5 +1,16 @@
 const rotateImage = (image) => {
-  return image;
+  let rotatedImage = [];
+  let l = image.length - 1;
+  for (let i = 0; i < image.length; i++) {
+    let k = 0;
+    for (let j = 0; j < image[i].length; j++) {
+      if (!rotatedImage[k]) rotatedImage[k] = [];
+      rotatedImage[k][l] = image[i][j];
+      k += 1;
+    }
+    l -= 1;
+  }
+  return rotatedImage;
 };
 
 module.exports = {
